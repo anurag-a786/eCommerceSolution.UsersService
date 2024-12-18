d4fd4bacc17f96607196128f4002f17d8f5c1eba
wickerknox <wickerknox89@gmail.com>
2024-12-17 18:21:46 -0800

    Users Microservice : Exception Handling Middleware

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -1,5 +1,6 @@
 using eCommerce.Infrastructure;
 using eCommerce.Core;
+using eCommerce.API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ builder.Services.AddControllers();
 
 // Build the web application
 var app = builder.Build();
+
+app.UseExceptionHandlingMiddleware();
 
 // Routing
 app.UseRouting();
