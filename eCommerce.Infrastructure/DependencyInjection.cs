d4d38291b074efbcc6cd8c8f6024cfb375617abe
wickerknox <wickerknox89@gmail.com>
2024-12-17 20:16:50 -0800

    Users Microservice : Users Repository

--- eCommerce.Infrastructure/DependencyInjection.cs
+++ eCommerce.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using eCommerce.Core.RepositoryContracts;
+using eCommerce.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace eCommerce.Infrastructure
 {
@@ -12,6 +14,8 @@ namespace eCommerce.Infrastructure
         {
             // TO DO : Add services to the IoC container
             // Infrastructure components often include data access, caching and other low-level components.
+
+            services.AddSingleton<IUsersRepository, UsersRepository>();
 
             return services;
         }
